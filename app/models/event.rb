d22f68180d9e980a68e0275f12b723c25f347bac
AshenNova <Kennerve14@gmail.com>
2020-11-12 23:01:14 +0800

    added validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  validates :Name_of_Event, presence: true, length: { maximum: 30 }
+  validates :Country, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :Description, presence: true, length: { minimum: 5, maximum: 1000 }
+  validates :Distance_participating, presence: true
 end
--- db/migrate/20201112064102_create_events.rb
+++ db/migrate/20201112064102_create_events.rb
@@ -1,11 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change #cant be the same as table name
     create_table :events do |t|
-      t.string :Name_of_Event #string for shorter text
-      t.string :Country
-      t.text :Description #text for longer text
-      t.string :Distance_participating
-      t.string :Date_of_Event
+      t.string :name_of_event #string for shorter text
+      t.string :country
+      t.text :description #text for longer text
+      t.string :distance_participating
+      t.string :date_of_event
     end
   end
 end
