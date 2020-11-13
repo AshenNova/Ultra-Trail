9bf7e596cf74eec8d8db16a9bcaed1013d56ee3c
AshenNova <Kennerve14@gmail.com>
2020-11-14 00:04:24 +0800

    validation and display error code with flash

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,25 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Event.all
   end
 
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+   @event = Event.new(params.require(:event).permit(:Name_of_Event, :Distance_participating, :Country, :Date_of_Event, :Description)) 
+    if @event.save
+        flash[:notice] = "Event was successfully submitted"
+        redirect_to @event
+     else
+       render 'new'
+     end
+  end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
+  
   validates :Name_of_Event, presence: true, length: { maximum: 30 }
   validates :Country, presence: true, length: { minimum: 2, maximum: 30 }
   validates :Description, presence: true, length: { minimum: 5, maximum: 1000 }
   validates :Distance_participating, presence: true
+  validates :Date_of_Event, presence: true, numericality: { only_integer: true }, length: { is: 8 }
+
 end
